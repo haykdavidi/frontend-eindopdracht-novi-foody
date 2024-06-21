2140b998003ea84fb1fee84d84b8d59e90f6b4c4
haykdavidi <152271658+haykdavidi@users.noreply.github.com>
2024-06-21 14:42:39 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,13 @@
-import {observer} from "mobx-react-lite";
-import {MenuItem, MenuItems, NavBarContainer, NavBarLogo} from "./styles.js";
-import logo from "../../assets/images/foody.png";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {BrowserRouter} from "react-router-dom";
+import App from "./App.jsx";
 
-function Navbar() {
-    return (
-        <NavBarContainer>
-            <NavBarLogo src={logo} />
-
-            <MenuItems>
-                <MenuItem to="/">Home</MenuItem>
-                <MenuItem to="/search">Search</MenuItem>
-                <MenuItem to="/whats-in-my-fridge">What's in my fridge?</MenuItem>
-                <MenuItem to="/my-recipes">Fave Recipes</MenuItem>
-                <MenuItem to="/Match your MoodFood">Match your MoodFood</MenuItem>
-                <MenuItem to="/decide-the-tempo">Decide the tempo</MenuItem>
-                <MenuItem to="/login">Login</MenuItem>
-            </MenuItems>
-        </NavBarContainer>
-    );
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
+);
