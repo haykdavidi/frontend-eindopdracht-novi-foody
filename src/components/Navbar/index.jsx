3953916e1285d0c9023994fa4a58afb13326e1a0
haykdavidi <152271658+haykdavidi@users.noreply.github.com>
2024-07-26 23:24:39 +0200

    Update index.jsx

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,23 +1,40 @@
-import {observer} from "mobx-react-lite";
-import {MenuItem, MenuItems, NavBarContainer, NavBarLogo} from "./styles.js";
+import { useUser } from "../../context/MainContext.jsx";
 import logo from "../../assets/images/foody.png";
+import { Button } from "react-bootstrap";
+import './navbar.css';
+import { Link } from "react-router-dom";
 
 function Navbar() {
-    return (
-        <NavBarContainer>
-            <NavBarLogo src={logo} />
+  const { authenticated, handleSignOut } = useUser();
 
-            <MenuItems>
-                <MenuItem to="/">Home</MenuItem>
-                <MenuItem to="/search">Search</MenuItem>
-                <MenuItem to="/whats-in-my-fridge">What's in my fridge?</MenuItem>
-                <MenuItem to="/my-recipes">My Recipes</MenuItem>
-                <MenuItem to="/questionnaire">Questionnaire</MenuItem>
-                <MenuItem to="/decide-the-tempo">Decide the tempo</MenuItem>
-                <MenuItem to="/login">Login</MenuItem>
-            </MenuItems>
-        </NavBarContainer>
-    );
+  const handleLogout = async () => {
+    try {
+      await handleSignOut();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="navbar-container">
+      <img className="navbar-logo" src={logo} alt="Logo" />
+      <div className="menu-items">
+        <Link className="menu-item" to="/">Home</Link>
+        <Link className="menu-item" to="/search">Search</Link>
+        <Link className="menu-item" to="/whats-in-my-fridge">What's in my fridge?</Link>
+        <Link className="menu-item" to="/questionnaire">Questionnaire</Link>
+        <Link className="menu-item" to="/decide-the-tempo">Decide the tempo</Link>
+        {authenticated ? (
+          <>
+            <Link className="menu-item" to="/my-recipes">My Recipes</Link>
+            <Button variant="danger" onClick={handleLogout}>Logout</Button>
+          </>
+        ) : (
+          <Link className="menu-item" to="/login">Login</Link>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default observer(Navbar);
+export default (Navbar);
