82390e3c13d1db4328d90726ab662128b3434597
haykdavidi <152271658+haykdavidi@users.noreply.github.com>
2024-07-26 23:23:15 +0200

    Update index.jsx

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,35 +1,27 @@
-import React from 'react'
-import {
-    DeleteRecipe,
-    RecipeActions,
-    RecipeBy,
-    RecipeCardContainer,
-    RecipeImage,
-    RecipeLink,
-    RecipeName,
-    SaveRecipe
-} from "./styles.js";
-import {userStore} from "../../stores/index.js";
-import {observer} from "mobx-react-lite";
+import React from 'react';
+import { useUser } from '../../context/MainContext.jsx';
+import { AiOutlineAppstoreAdd, AiOutlineDelete } from "react-icons/ai";
+import "./card.css";
 
-function RecipeCard({rec}) {
-    return (
-        <RecipeCardContainer>
-            <RecipeImage src={rec.image} alt={rec.label}/>
-            <RecipeName>{rec.label}</RecipeName>
+function RecipeCard({ rec }) {
+  const { containsRecipe, addRecipe, removeRecipe } = useUser();
 
-            <RecipeBy>by {rec.source}</RecipeBy>
-            <RecipeActions>
-                <RecipeLink href={rec.url} target="_blank">View Recipe</RecipeLink>
-                {!userStore.containsRecipe(rec) && (
-                    <SaveRecipe onClick={() => userStore.addRecipe(rec)} size={30} />
-                )}
-                {userStore.containsRecipe(rec) && (
-                    <DeleteRecipe onClick={() => userStore.removeRecipe(rec)} size={30} />
-                )}
-            </RecipeActions>
-        </RecipeCardContainer>
-    );
+  return (
+    <div className="recipe-card-container">
+      <img className="recipe-image" src={rec.image} alt={rec.label} />
+      <h4 className="recipe-name">{rec.label}</h4>
+      <p className="recipe-by">by {rec.source}</p>
+      <div className="recipe-actions">
+        <a className="recipe-link" href={rec.url} target="_blank" rel="noopener noreferrer">View Recipe</a>
+        {!containsRecipe(rec) && (
+          <AiOutlineAppstoreAdd className="save-recipe" onClick={() => addRecipe(rec)} size={30} />
+        )}
+        {containsRecipe(rec) && (
+          <AiOutlineDelete className="delete-recipe" onClick={() => removeRecipe(rec)} size={30} />
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default observer(RecipeCard);
+export default RecipeCard;
